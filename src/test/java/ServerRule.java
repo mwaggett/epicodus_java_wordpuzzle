9499a1dfee63aec22be52a9d1526cce1d2df0e0b
mwaggett <mwaggett@alumni.reed.edu>
2015-08-14 11:08:28 -0700

    Change to contain actual App name.

--- src/test/java/ServerRule.java
+++ src/test/java/ServerRule.java
@@ -5,7 +5,7 @@ public class ServerRule extends ExternalResource {
 
   protected void before() {
       String[] args = {};
-      Coin.main(args);
+      Wordpuzzle.main(args);
    }
 
   protected void after() {
